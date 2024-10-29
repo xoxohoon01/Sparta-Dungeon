0dbe05b7e6b8a4b3d6c382cfa0abb6ac19551a32
BL4CK DoG <xoxohoon01@gmail.com>
2024-10-29 10:33:25 +0900

    feat: 아이템 지속시간 추가

--- Assets/Scripts/Character/EnterObject.cs
+++ Assets/Scripts/Character/EnterObject.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnterObject : MonoBehaviour
 {
+    IEnumerator speedUpCoroutine;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Interactable"))
@@ -16,11 +17,20 @@ public class EnterObject : MonoBehaviour
                 case ObjectType.Item:
                     if (other.GetComponent<ItemScript>().data.name == "¼Óµµ ¾÷")
                     {
+                        if (speedUpCoroutine != null) StopCoroutine(speedUpCoroutine);
                         GetComponent<PlayerStatus>().moveSpeed = 10.0f;
+                        speedUpCoroutine = SpeedUp(other.GetComponent<ItemScript>().data.duration);
+                        StartCoroutine(speedUpCoroutine);
                         Destroy(other.gameObject);
                     }
                     break;
             }
         }
+    }
+
+    IEnumerator SpeedUp(float _duration)
+    {
+        yield return new WaitForSeconds(_duration);
+        GetComponent<PlayerStatus>().moveSpeed = 5.0f;
     }
 }
--- Assets/Scripts/Item/ItemData.cs
+++ Assets/Scripts/Item/ItemData.cs
@@ -8,4 +8,5 @@ public class ItemData : ScriptableObject
 {
     public string name;
     public string description;
+    public float duration;
 }
